99564e269d8070c9664ef900c942b87203267025
Laura <theimer.laura@gmail.com>
2019-12-04 23:00:50 -0500

    Have testing for display done.  Snapshot.  Working with Dashboard, and Controls some more.

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,13 @@
 // Test away!
+
+import React from "react";
+import { render } from "@testing-library/react";
+
+
+import Controls from "./Controls.js";
+
+test( "Controls are present", () => {
+    const wrapper = render(<Controls />);
+
+    expect(wrapper).getByTestId();
+}); 
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,18 @@
 // Test away
+
+import React from 'react';
+import { render } from "@testing-library/react";
+import Dashboard from './Dashboard.js'; 
+
+// test("Shows Controls for the Dashboard of the gate", () => {
+//     const wrapper = render(<Dashboard />);
+//     const LockGate = wrapper.getByPlaceholderText(/'Lock Gate'/i);
+//     const CloseGate = wrapper.getByPlaceholderText(/'Close Gate'/i);
+
+//     expect(LockGate).toBeVisble(); 
+//     expect(CloseGate).toBeVisble();
+// });
+
+// test("Shows Controls for the Dashboard", async () => {
+//     const wrapper = render(<Dashboard />); 
+// });
